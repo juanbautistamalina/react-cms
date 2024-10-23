62f8c842ecf557a1136728e9c959ea816f5b7e13
Juan Bautista Malina <malinajuanbautista@gmail.com>
2024-10-23 18:08:11 -0300

    Control de todas las funcionalidades. Implementación de agregado de usuarios a la tabla. Reimplementación de eliminación de usuarios en la tabla.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { FormEvent, useRef, useState } from "react";
+import { useState } from "react";
 import Form from "./components/Form";
 import Table from "./components/Table";
 import Button from "./components/Button";
 
 function App() {
-  const contacts = [
+  const initialContacts = [
     {
       name: "Juan Bautista",
       lastname: "Malina",
@@ -19,38 +19,44 @@ function App() {
       name: "David",
       lastname: "Goggins",
       email: "davidgoggins@gmail.com",
-    }
+    },
   ];
 
-  const [user, setUser] = useState({
-    name: "nombre",
-    lastname: "apellido",
-    email: "email@ejemplo.com"
-  });
 
+  // Variable para limpiar campos
   const userClear = {
     name: "",
     lastname: "",
-    email: ""
-  }
+    email: "",
+  };
 
-  const [newContact, setNewContact] = useState(contacts);
   
-  // Función que se ejecuta al enviar el formulario
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault(); 
-    
-    setNewContact([...newContact, user]);
-    console.log(newContact)
-};
+  // Variables de estado del usuario (Formulario)
+  const [user, setUser] = useState(userClear);
 
+  // Variables de estado del contacto (Tabla)
+  const [contacts, setContacts] = useState(initialContacts);
+
+
+
+  // Función que se ejecuta al presionar el botón de Enviar
+  const handleSubmit = () => {
+    setContacts([user, ...contacts]); // Agregar el nuevo usuario
+  };
+
+  // Función que se ejecuta al presionar sobre los elementos de la lista (borrar items)
+  const handleDelete = (index: number) => {
+    const newContact = [...contacts]; // Creo una copia de la variable de estado
+    newContact.splice(index, 1); // Elimino el elemento presionado del array 
+    setContacts(newContact); // Actualizo la variable de estado (contacts)
+  };
 
   return (
     <div className="container">
-      <Form user={user} onSubmit={handleSubmit} setUser={setUser}/>
-      <Button onClick={handleSubmit} send={true} >Enviar</Button>
+      <Form user={user} setUser={setUser} />
+      <Button onClick={handleSubmit} send={true}>Enviar</Button>
       <Button onClick={() => setUser(userClear)}>Limpiar</Button>
-      <Table data={newContact}/>
+      <Table data={contacts} onDelete={handleDelete}/>
     </div>
   );
 }
